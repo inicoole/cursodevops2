379e6e8af16920e516699b2dca386fc690dabb54
nicolesepulveda <nicole.sepulvedac@idiem.cl>
2024-05-09 16:10:00 -0400

    test199

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { dividir, multiplicar, restar, sumar } from "./calcular.js";
-
+import fs from "fs";
 const AMBIENTE = process.env.AMBIENTE ?? "desconocido";
 const API_KEY = process.env.API_KEY ?? '';
 
@@ -30,7 +30,8 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api", (req, res) => {
-  res.send(API_KEY);
+  const miVariable = fs.readFileSync(API_KEY, 'utf8');
+  res.send(miVariable);
 });
 
 export default app;
